6c1b01b6c102ad7a01466b12a3772921c8eb13aa
Long <lyokha113@gmail.com>
2019-07-23 05:42:45 +0700

    Fix api

--- src/main/java/com/cloud/auction/model/Offer.java
+++ src/main/java/com/cloud/auction/model/Offer.java
@@ -1,10 +1,10 @@
 package com.cloud.auction.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.codehaus.jackson.annotate.JsonIgnore;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
